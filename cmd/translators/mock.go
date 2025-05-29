f9861b565f664a6bdf5684d2f23bf82db647bfa3
jay <jay@lantern.io>
2025-05-28 20:19:12 -0600

    add mocks

---
+++ cmd/translators/mock.go
@@ -0,0 +1,22 @@
+package translators
+
+import (
+	"context"
+	"fmt"
+)
+
+type mockTranslator struct{}
+
+func (m *mockTranslator) SupportedLanguages(ctx context.Context, baseLang string) ([]string, error) {
+	return []string{"en", "es"}, nil
+}
+
+func (m *mockTranslator) Translate(ctx context.Context, targetLang string, texts []string) ([]string, error) {
+	translations := make([]string, len(texts))
+	for i, text := range texts {
+		translations[i] = fmt.Sprintf("this would be %q, but in %s", text, targetLang)
+	}
+	return translations, nil
+}
+
+func (m *mockTranslator) Close(ctx context.Context) {}
